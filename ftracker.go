b453baa22d59b8830bd7d40add7b2476674a8ff0
LozovoyArtem <Lozovoy1999@list.ru>
2024-05-12 16:48:53 +0300

    third commit

--- ftracker.go
+++ ftracker.go
@@ -101,7 +101,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	return float64((walkingCaloriesWeightMultiplier*weight+((math.Pow(meanSpeed(action, duration)/3.6, 2))/(height/cmInM))*walkingSpeedHeightMultiplier*weight)*duration*minInH) + 0.07
+	return float64((walkingCaloriesWeightMultiplier*weight + ((math.Pow(meanSpeed(action, duration)*kmhInMsec, 2))/(height/cmInM))*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
